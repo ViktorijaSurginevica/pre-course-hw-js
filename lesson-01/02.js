422208e92a981a1922d65f3ec5b2d7888e8daa33
Viktorija Surginevica <viafuture89@gmail.com>
2024-01-03 17:59:10 +0300

    Number

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -8,5 +8,5 @@ let number = Math.floor(Math.random() * 100);
 if (number % 2 === 0) {
     console.log(number, 'четное число');
 } else {
-console.log(number, 'нечетное число');
+console.log(number, 'нечетное числоо');
 }
