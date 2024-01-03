d5ebc6c3a94c1e181f61f5777f41be9e5bdbe867
Viktorija Surginevica <viafuture89@gmail.com>
2024-01-03 17:51:54 +0300

    Math.random

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -2,6 +2,11 @@
 //Math.floor(Math.random() * 100)
 //  а затем проверь их выводя в консоль: «[вставь сюда полученное число] :Это четное число» или «[вставь сюда полученное число] :Это нечетное число». С каждой перезагрузкой страницы у тебя будет новое число.​
 
-let number = Math.floor(Math.random() * 50)*2;
-let number1 = Math.floor(Math.random() * 50)*2-1;
-console.log(number, 'Это четное число'), (number, 'Это нечетное число')
+
+
+let number = Math.floor(Math.random() * 100);
+if (number % 2 === 0) {
+    console.log(number, 'четное число');
+} else {
+console.log(number, 'нечетное число');
+}
