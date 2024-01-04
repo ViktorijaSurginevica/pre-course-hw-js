067135101da00bbb1a8abde2e4a888525d58c652
Viktorija Surginevica <viafuture89@gmail.com>
2024-01-04 08:57:53 +0300

    Increment and decrement

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,7 @@
-​let number = 1;
+//​Получи в консоли 1,3,3,1 
+let number = 1;
 
-console.log(number**); 
-console.log(**number); 
-console.log(number**); 
-console.log(**number); 
+console.log(number++); 
+console.log(++number); 
+console.log(number++); 
+console.log(--number); 
