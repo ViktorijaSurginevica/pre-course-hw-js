24fff3a928762860e73fd0fae4fff468c09f21f6
Viktorija Surginevica <viafuture89@gmail.com>
2024-01-10 17:25:05 +0300

    Commit

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,4 +5,4 @@
 
 
 let number1 = Math.floor(Math.random() * 100);
-if (number1 % 2 === 0) {console.log(number1, 'Это четное число');} else {console.log(number1, 'Это нечетное число');}
+if (number1 % 2 === 0) {console.log(number1, 'Это четное числоо');} else {console.log(number1, 'Это нечетное число');}
